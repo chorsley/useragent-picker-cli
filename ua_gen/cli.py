37a2c4b447da15c33b9afc8f214183dfc2ed9b39
Chris Horsley <chris.horsley@cosive.com>
2020-07-20 10:54:59 +1000

    Add force_db_update arg to UAGen, otherwise pass responsibility for deciding to update to UADBManager

--- ua_gen/cli.py
+++ ua_gen/cli.py
@@ -47,11 +47,7 @@ class NoUserAgentFoundException(Exception):
 
 def main(args):
     """Generates random but realistic user agents on a command line (or via API)"""
-    if (args["--force-update-db"]):
-        ua_db = UADBManager()
-        ua_db.fetch_db()
-
-    uagen = UAGen()
+    uagen = UAGen(force_db_update=bool(args["--force-update-db"]))
 
     try:
         print(uagen.get_ua(aliases=args["FILTER"]))
@@ -61,16 +57,133 @@ def main(args):
         logger.error("you might need to go and find it elsewhere.") 
         sys.exit(1)
 
+class UADBManager(object):
+    """
+    Manages downloading, enriching, updating, and returning the master list
+    of user agent definitions.
+
+    The original UA definitions file has some inaccuracies, notably listing
+    the vendor as Google everywhere, including for IE. Since it has valuable
+    weightings as seen in a user population, we instead do some of our own
+    user agent parsing with user_agents for higher accuracy.
+    """
+    def __init__(self, force_db_update=False):
+        self.db_dir = os.path.join(os.path.expanduser("~"), ".ua-gen-cli/")
+        self.raw_db_file_path = os.path.join(self.db_dir, "raw_ua_db.json")
+        self.db_file_path = os.path.join(self.db_dir, "enriched_ua_db.json")
+
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+        logger.debug(f"DB file: {self.db_file_path}")
+        if (force_db_update or self.no_db_file_present or self.is_too_old):
+            self.fetch_db()
+
+        self.ua_db = []
+
+    def fetch_db(self):
+        logger.error("Fetching and enriching user agent DB - we'll be much faster on the next run, scout's honour!")
+
+        r = requests.get(UA_DB_URL, stream=True)
+
+        if r.ok:
+            with open(self.raw_db_file_path, "wb") as f:
+                r.raw.decode_content = True
+                gzip_file = gzip.GzipFile(fileobj=r.raw)
+                shutil.copyfileobj(gzip_file, f)
+
+        self.enrich_db()
+
+    def enrich_db(self):
+        # the DB file we get is very useful, but inaccurate in places
+        # for OS and the like.
+        # We enrich using a UA parsing lib for better consistency.
+
+        wf = open(self.db_file_path, 'w')
+
+        entries = []
+
+        with open(self.raw_db_file_path) as rf:
+            for entry in json.load(rf):
+                parsed_ua = ua_parse(entry.get("userAgent"))
+                entry = self.update_ua_db_entry(entry, parsed_ua)
+                entries.append(entry)
+
+            wf.write(json.dumps(entries))
+
+        wf.close()
+
+    def update_ua_db_entry(self, entry, parsed_ua):
+        entry['browser_family'] = parsed_ua.browser.family
+        entry['platform'] = parsed_ua.os.family
+        entry['deviceCategory'] = self._get_device_category(parsed_ua)
+
+        # we need a more granular setting, so we split some compound family labels up
+        if entry['browser_family'] == "Chrome Mobile":
+            entry['browser_family'] = "Chrome"
+            entry['deviceCategory'] = "mobile"
+        if entry['browser_family'] == "Chrome Mobile iOS":
+            entry['browser_family'] = "Chrome"
+            entry['deviceCategory'] = "mobile"
+            entry['platform'] = "iOS"
+        if entry['browser_family'] == "Chrome Mobile WebView":
+            entry['browser_family'] = "Chrome"
+            entry['deviceCategory'] = "mobile"
+        if entry['browser_family'] == "Edge Mobile":
+            entry['browser_family'] = "Edge"
+            entry['deviceCategory'] = "mobile"
+        elif entry['browser_family'] == "Mobile Safari":
+            entry['browser_family'] = "Safari"
+            entry['deviceCategory'] = "mobile"
+        elif entry['browser_family'] == "QQ Browser Mobile":
+            entry['browser_family'] = "QQ Browser"
+            entry['deviceCategory'] = "mobile"
+        elif entry['browser_family'] == "UC Browser Mobile":
+            entry['browser_family'] = "UC Browser"
+            entry['deviceCategory'] = "mobile"
+        elif entry['browser_family'] == "Firefox iOS":
+            entry['browser_family'] = "Firefox"
+            entry['deviceCategory'] = "mobile"
+            entry['platform'] = "iOS"
+
+        return entry
+
+    @property
+    def is_too_old(self):
+        try:
+            stat = os.stat(self.db_file_path)
+        except FileNotFoundError:
+            return True   # actually doesn't exist, but that's too old for our purposes
+        epoch_now = int(time.time())
+
+        return((epoch_now - stat.st_mtime) > UA_DB_STALE_SECONDS)
+
+    @property
+    def no_db_file_present(self):
+        return not os.path.isfile(self.db_file_path)
+
+    def load_ua_defs(self):
+        with open(self.db_file_path) as f:
+            for entry in json.load(f):
+                self.ua_db.append(entry)
+        return(self.ua_db)
+
+    def _get_device_category(self, parsed_ua):
+        if parsed_ua.is_mobile:
+            return 'mobile'
+        elif parsed_ua.is_pc:
+            return 'desktop'
+        elif parsed_ua.is_tablet:
+            return 'tablet'
+        elif parsed_ua.is_bot:
+            return 'bot'
+
 
 class UAGen(object):
-    def __init__(self, ua_db=None):
+    def __init__(self, ua_db:UADBManager=None, force_db_update:bool=False):
         # normal operation
         if not ua_db:
-            self.db_manager = UADBManager()
-            if (self.db_manager.is_too_old()):
-                logger.info("UA DB stale, fetching a fresh one...")
-                self.db_manager.fetch_db()
-
+            self.db_manager = UADBManager(force_db_update=force_db_update)
             self.ua_db = self.db_manager.load_ua_defs()
         # usually for testing
         else:
