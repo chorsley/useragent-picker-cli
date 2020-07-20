a631a1801c498d0d211e1568303c3cfa33d44e67
Chris Horsley <chris.horsley@cosive.com>
2020-07-20 10:52:10 +1000

    New rules for mobile and FB browsers

--- ua_gen/ua_rules.py
+++ ua_gen/ua_rules.py
@@ -7,6 +7,7 @@ ua_rules = [
     {'match': 'mobile', 'aliases': ['mobile', 'mob'], 'cat': 'deviceCategory'},
     {'match': 'desktop', 'aliases': ['desktop', 'desk'], 'cat': 'deviceCategory'},
     {'match': 'tablet', 'aliases': ['tablet', 'tab'], 'cat': 'deviceCategory'},
+    
     {'match': 'Firefox', 'aliases': ['netscape', 'firefox', 'ff'], 'cat': 'browser_family'},
     {'match': 'IE', 'aliases': ['ie', 'msie', 'iexplore', 'internet explorer'], 'cat': 'browser_family'},
     {'match': 'Edge', 'aliases': ['edge'], 'cat': 'browser_family'},
@@ -14,9 +15,16 @@ ua_rules = [
     {'match': 'Safari', 'aliases': ['safari'], 'cat': 'browser_family'},
     # Chrome, which we have to define more specifically since everyone has in agent
     {'match': 'Chrome', 'aliases': ['chrome'], 'cat': 'browser_family'},
+    {'match': 'Chromium', 'aliases': ['chromium'], 'cat': 'browser_family'},
+    {'match': 'Facebook', 'aliases': ['facebook', 'fb'], 'cat': 'browser_family'},
+    {'match': 'UC Browser', 'aliases': ['uc', 'ucbrowser'], 'cat': 'browser_family'},
+    {'match': 'QQ Browser', 'aliases': ['qq', 'qqbrowser'], 'cat': 'browser_family'},
+    
     {'match': 'Windows', 'aliases': ['win32', 'win', 'windows'], 'cat': 'platform'},
     {'match': 'Mac OS X', 'aliases': ['macintel', 'osx', 'Mac', 'MacOS'], 'cat': 'platform'},
     {'match': 'Linux', 'aliases': ['linux', 'x11'], 'cat': 'platform'},
+    {'match': 'iOS', 'aliases': ['ios', 'iphone'], 'cat': 'platform'},
+    {'match': 'Android', 'aliases': ['android'], 'cat': 'platform'},
 ]
 
 __all__ = ['ua_rules']
