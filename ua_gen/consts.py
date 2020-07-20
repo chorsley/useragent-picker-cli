30d09ca35793ed872f90588d6b4d637c35745ee2
Chris Horsley <chris.horsley@cosive.com>
2020-07-20 17:51:31 +1000

    Add category labels for nicer filter list output

--- ua_gen/consts.py
+++ ua_gen/consts.py
@@ -1,8 +1,15 @@
 UA_DB_URL = 'https://raw.githubusercontent.com/intoli/user-agents/master/src/user-agents.json.gz'
 UA_DB_STALE_SECONDS = 24 * 60 * 60 * 30
 
-UA_BROWSER_FAMILY = "browser_family"
+UA_BROWSER_FAMILY = "browserFamily"
 UA_PLATFORM = "platform"
 UA_USERAGENT = "userAgent"
 UA_DEVICE_CATEGORY = "deviceCategory"
+
+UA_PROP_LABELS = {
+    UA_BROWSER_FAMILY: 'Browser family',
+    UA_PLATFORM: 'Platform',
+    UA_USERAGENT: 'User agent',
+    UA_DEVICE_CATEGORY: 'Device category'
+}
 
