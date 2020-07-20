38421b0e1174955748db32994f8d23debe182310
Chris Horsley <chris.horsley@cosive.com>
2020-07-20 17:52:57 +1000

    Add initial tests for CLI arg parsing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,27 +1,61 @@
 import pytest
 from ua_gen import cli
+from ua_gen import consts as c
+
+import subprocess
+
+def capture(command):
+    proc = subprocess.Popen(command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    out,err = proc.communicate()
+    return out, err, proc.returncode
+
+def test_anagram_no_param():
+    """
+    Should produce a simple UA on no args
+    """
+    command = ["python3", "dev_runner.py"]
+    out, _, exitcode = capture(command)
+    assert exitcode == 0
+    assert out.decode('utf-8').count('\n') == 1
+    assert len(out) > 20
+    assert len(out) < 200
 
 
-@pytest.mark.skip
-def test_cli(runner):
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert not result.exception
-    for _ in range(1, 1000):
-        assert len(result.output.strip()) > 10
+def test_list_filters():
+    """
+    Should produce a simple UA on no args
+    """
+    command = ["python3", "dev_runner.py", "--list-filters"]
+    out, _, exitcode = capture(command)
+    outd = out.decode('utf-8')
+    assert exitcode == 0
+    assert outd.count('\n') > 10
+    assert c.UA_PROP_LABELS[c.UA_PLATFORM] in outd
 
 
-@pytest.mark.skip
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
+def test_filter():
+    """
+    Should produce a simple UA on no args
+    """
+    command = ["python3", "dev_runner.py", "Chrome"]
+    out, _, exitcode = capture(command)
+    outd = out.decode('utf-8')
+    assert exitcode == 0
+    assert outd.count('\n') == 1
+    assert 'Chrome' in outd
 
 
-@pytest.mark.skip
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Chris'])
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, Chris.'
+def test_filters():
+    """
+    Should produce a simple UA on no args
+    """
+    command = ["python3", "dev_runner.py", "Chrome", "Linux"]
+    out, _, exitcode = capture(command)
+    outd = out.decode('utf-8')
+    assert exitcode == 0
+    assert outd.count('\n') == 1
+    assert 'Chrome' in outd
+    assert 'Linux' in outd
